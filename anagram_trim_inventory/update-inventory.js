8ccce0b1307b6e81e78334defffbaa274e12cd21
tajohnson0316 <armanijohnson1331@gmail.com>
2023-04-17 20:41:37 -0500

    Add isAnagram, trim, and update inventory algos

---
+++ anagram_trim_inventory/update-inventory.js
@@ -0,0 +1,57 @@
+/* 
+  Given an array of objects / dictionaries to represent new inventory,
+  and an array of objects / dictionaries to represent current inventory,
+  update the quantities of the current inventory
+  
+  if the item doesn't exist in current inventory, add it to the inventory
+  return the current inventory after updating it.
+*/
+const currInv1 = [
+  { name: "Peanut Butter", quantity: 20 },
+  { name: "Grain of Rice", quantity: 1 },
+];
+const newInv1 = [
+  { name: "Grain of Rice", quantity: 9000 },
+  { name: "Peanut Butter", quantity: 50 },
+  { name: "Royal Jelly", quantity: 20 },
+];
+const expected1 = [
+  { name: "Peanut Butter", quantity: 70 },
+  { name: "Grain of Rice", quantity: 9001 },
+  { name: "Royal Jelly", quantity: 20 },
+];
+
+const newInv2 = [];
+const currInv2 = [{ name: "Peanut Butter", quantity: 20 }];
+const expected2 = [{ name: "Peanut Butter", quantity: 20 }];
+
+const newInv3 = [{ name: "Peanut Butter", quantity: 20 }];
+const currInv3 = [];
+const expected3 = [{ name: "Peanut Butter", quantity: 20 }];
+
+/**
+ * @typedef {Object} Inventory
+ * @property {string} Inventory.name The name of the item.
+ * @property {number} Inventory.quantity The quantity of the item.
+ */
+
+/**
+ * Updates the current inventory based on the new inventory.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<Inventory>} newInv A shipment of new inventory.
+ *    An array of inventory objects.
+ * @param {Array<Inventory>} currInv
+ * @return The currInv after being updated.
+ */
+function updateInventory(newInv, currInv) {
+  for (newItem in newInv) {
+    for (currentItem in currInv) {
+      if (newItem.name == currentItem.name) {
+        currentItem.quantity += newItem.quantity;
+      } else {
+      }
+    }
+  }
+  return currInventory;
+}
